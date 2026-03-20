36ae69d1014a8b1df1228ec4be1070a62d86d9a0
Jaemin Lee <ljmsp83@naver.com>
2026-03-20 17:45:36 +0900

    $npm_config_message

--- src/html/main.js
+++ src/html/main.js
@@ -137,14 +137,7 @@ ${head(data)}
     
     </main>
 
-    <footer role="contentinfo">
-        <div class="copy">&copy; 2026 CHKIP.ORG  Built with <a href="https://cloudflare.com" target="_blank">cloudflare.com</a></div>
-        <ul>
-          <li><a href="/about">About</a></li>
-          <li><a href="/terms">Terms of Service</a></li>
-          <li><a href="/policy">Privacy Policy</a></li>
-        </ul>
-    </footer>
+    ${footer()}
   </div>
 
   <script>
